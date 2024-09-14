822fd5c3be9d3a3dc5422ccdc3531ebe5a4ec014
Jocho14 <jochotyler1@gmail.com>
2024-09-14 18:12:13 +0200

    test: checking if failing test on CI won't merge to repo

--- tests/src/databaseTests.test.ts
+++ tests/src/databaseTests.test.ts
@@ -2,12 +2,15 @@ import "module-alias/register";
 import { client } from "@/tests/src/databaseConfig";
 import { prepareDatabase } from "@/tests/src/databaseConfig";
 
-beforeEach(async () => {
+beforeAll(async () => {
   await client.connect();
+});
+
+beforeEach(async () => {
   await prepareDatabase();
 });
 
-afterEach(async () => {
+afterAll(async () => {
   await client.end();
 });
 
@@ -17,6 +20,17 @@ describe("Database insert test", () => {
       await client.query(
         `INSERT INTO users (first_name, last_name, phone_number, date_of_birth) 
           VALUES ('John', 'Doe', '1234567890', '1990-01-01')`
+      )
+    ).resolves;
+  });
+
+  test("Insert data into the database fail", async () => {
+    expect(
+      await client.query(
+        `INSERT INTO users (first_name, last_name, phone_number, date_of_birth) 
+          VALUES ('John', 'Doe', '1234567890', '1990-01-01');
+        INSERT INTO users (first_name, last_name, phone_number, date_of_birth) 
+        VALUES ('John', 'Doe', '1234567890', '1990-01-01');`
       )
     ).resolves;
   });
