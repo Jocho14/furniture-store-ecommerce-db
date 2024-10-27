4720627f0c4273108cd109f4a1257374f757d8f7
Jocho14 <jochotyler1@gmail.com>
2024-10-27 02:46:22 +0200

    feat: added initial warehouse

--- src/data/initial_data.sql
+++ src/data/initial_data.sql
@@ -1,2 +1,1 @@
-INSERT INTO accounts (user_id, email, password_hash) VALUES (1, 'example@email.com', 'password');
-INSERT INTO products (name, price, description) VALUES ('krzeslo', 199.99, 'kozackie krzeslo');
+INSERT INTO warehouses (name, location) VALUES ('Magazyn1', 'ul. przestrzenna 1');
